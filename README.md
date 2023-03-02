d3d786bcf490b44af3825f50313f9ea70f3f6866
Zach Radabaugh <zach.radaba@gmail.com>
2023-03-01 20:56:12 -0700

    Initial commit for this branch. Readme has notes

--- README.md
+++ README.md
@@ -1,31 +1,26 @@
 # N-Able Report Manager Extraction Tool
-This program is a data processing tool designed to import reports sent by N-Able's Report Manager, clean up the data, extract specific tables from the data, and save the extracted data to an XLSX file. The program uses the Python programming language and several popular Python libraries for data processing, including pandas and tkinter.
+This program is a data processing tool designed to import reports sent by N-Able's Report Manager, clean up the data, and exports the extracted data to MariaDB. The program uses the Python programming language and several popular Python libraries for data processing, including pandas and tkinter.
 
 ## Getting Started
 ### Prerequisites
 * Python 3.6 or higher
 * pandas
 * tkinter
-* XlsxWriter
-* Openpyxl
 * TQDM
 
 ## Usage
-* Put all of the XLSX files you want to process in a single directory.
+* Put all of the CSV files you want to process in a single directory.
 * Run the script and select the directory containing the files when prompted.
-* Enter customer names to be filtered out in the filter.py file.
-* Select a file path and name for the output file.
-* The script will process the files and generate a new Excel file with the merged data.
-* The script will prompt the user if they want to also save data to a single excel file where data from many reports can be used for long-term data analysis
+* The script will process the files and export the data to a DB using the configuration in config.py.
 
 ## Built With
 * Python - The programming language used
 * pandas - Library used for data manipulation
 * tkinter - Library used for GUI development
-* XlsxWriter - Library used for Excel file generation
-* Openpyxl - Library used to read Excel file
 * TQDM - Libray used to display progress
 
+## Branch Notes
+As I found a better export option in N-Able which saves in csv format and also includes all of the individual patch information which is helpful, I've rewritten this script to just pull in the data from the CSV file and export the data to a DB. It is much simpler this way, however at the moment, it doesn't export out a summary xlsx file. This likely will be added in at a later stage.
 
 ### Authors
 Zach Radabaugh (zach.radaba@gmail.com)
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-# Import required modules
+# Import require modules
 import tkinter as tk
 from tkinter import filedialog
 from pathlib import Path
 import pandas as pd
-import mariadb
 import os
-from filter import customer_filter
-from config import db_config
+import mariadb
 from tqdm import tqdm
+from config import db_config
 
 # Create a Tkinter root window
 root = tk.Tk()
@@ -16,203 +15,66 @@ root.withdraw()
 # Define global variables
 source = str
 date = str
-type = str
-master_df = pd.DataFrame(columns=['Date', 'Source', 'Type', 'Customer', 'Device Class', 'Device Name', 'Patch Category', 'Patch Status', 'Count'])
+columns = ['Customer_Name', 'Installation_Status', 'Device_Class', 'Device_Name', 'Patch_Name', 'Patch_Products', 'Patch_Category', 'Publish_Date', 'Approval_Status', 'Approval_Date', 'Status_Change_Date']
+master_df = pd.DataFrame(columns=columns)
 
-# Prompt user to select directory containing XLSX files
+# Prompt user to select directory containing CSV files
 def get_dir():
-    """
-    Prompts user to select the directory containing XLSX files.
-    Args:
-    None
 
-    Returns:
-    dir_path (string): The path of the selected directory.
-    """
     default_directory = str(Path.home().joinpath('Desktop'))
-    dir_path = filedialog.askdirectory(initialdir=default_directory, title='Select Directory Containing XLSX Files')
+    dir_path = filedialog.askdirectory(initialdir=default_directory, title='Select Directory Containing CSV Files')
 
-    print("Using directory path", dir_path)
+    print(f"Using directory path, {dir_path}")
 
     return dir_path
 
-# Function to import the Excel file
-def import_excel(file):
-    """
-    Imports an Excel file and sets variables based on the filename.
-
-    Args:
-    file_path (string): The path of the Excel file.
-
-    Returns:
-    df (Pandas dataframe): The dataframe containing the data from the Excel file.
-    source(string): The automatically pulled server source taken from the filename.
-    type(string): The automatically pulled endpoint type taken from the filename.
-    date(string):The automatically pulled date taken from the filename.
-    """
-
+# Import CSV File Function
+def import_csv(file):
     # Set variables from the filename
 
     source = file.split(' ')[0] # Split the filename by the first space, get the item before
-    type = file.split(' ')[1] # Split the filename by the first space, get the item after
     date1 = file.split('Patch Status')[1] # Split the filename by "Patch Status", take the word item after
     date = date1.split('T')[0] # Split the date by the T, take the item before
-    print("Pulling information from the filename, found the source server is" , source , ". The endpoint type is" , type , ". The full date is" , date1 , " and the core date is" , date , ".")
-    
-    # Load the Excel file into a pandas dataframe
-    df = pd.read_excel(file_path, sheet_name=None)
-    return df, source, type, date
+    print(f"Pulling information from the filename: Server is {source}, date is {date}")
 
-# Function to clean up each sheet
-def clean_data(df):
-    """
-    Cleans up the data in each sheet of the input dataframe.
+    import_df = pd.read_csv(file_path, header=None, names=columns, usecols=range(11))
 
-    Args:
-    df (Pandas dataframe): The dataframe containing the data from the Excel file.
+    return import_df, source, date
 
-    Returns:
-    df (Pandas dataframe): The cleaned dataframe.
-    """
-    # Remove the first 5 sheets of the dataframe
-    print("Cleaing up data")
-    for i in range(5):
-        df.pop(list(df.keys())[0])
+# Clean Data Function
+def cleanup_df(import_df, source, date):
+    # Look for valid data by finding where there is data in column 7
+    row_index = import_df[import_df.iloc[:, 7].notnull()].index[0]
+    # Drop all rows above row_idex (including row_index)
+    clean_df = import_df.drop(import_df.index[:row_index + 1])
+    # Remove the Customer: text from the Customer_Name Column
+    clean_df['Customer_Name'] = clean_df['Customer_Name'].str.replace('Customer: ', '')
+    # Remove the Patch Status: text from the Customer_Name Column
+    clean_df['Installation_Status'] = clean_df['Installation_Status'].str.replace('Patch Status: ', '')
+    # Reset the row index
+    clean_df = clean_df.reset_index(drop=True)
+    # Add a column with the server source
+    clean_df = clean_df.assign(Source=source) 
+    # Add a column with the date of this report
+    clean_df = clean_df.assign(Report_Date=date) 
 
-    for sheet_name in df:
-        # Clean up data of Sheet 6
-        if sheet_name == 'Sheet6':
-            customer_name = df[sheet_name].iloc[1, 1].split(":")[1].strip() # Find the customer name
-            df[sheet_name].columns = df[sheet_name].iloc[4] # Set column values to row 4
-            df[sheet_name] = df[sheet_name].drop(columns=[df[sheet_name].columns[0]]) # Drop first column
-            df[sheet_name] = df[sheet_name].assign(Customer=customer_name) # Create new column called Customer Name, assign value of customer_name
+    # Format the Publish Date and Approval Date columns using YYYY-MM-DD instead of Feb 16, 2023
+    clean_df['Publish_Date'] = pd.to_datetime(clean_df['Publish_Date'], errors='coerce')
+    clean_df['Approval_Date'] = pd.to_datetime(clean_df['Approval_Date'], errors='coerce')
+    clean_df['Status_Change_Date'] = pd.to_datetime(clean_df['Status_Change_Date'], errors='coerce')
 
-        # Clean up the data for other sheets
-        else:
-            customer_name = df[sheet_name].columns[1] # Set customer_name to the value of the B1 cell
-            df[sheet_name].columns = df[sheet_name].iloc[2] # Set column values to row 2
-            df[sheet_name] = df[sheet_name].drop(columns=[df[sheet_name].columns[0]]) # Drop first column
-            customer_name = customer_name[10:] # Remove the text "Customer: "
-            df[sheet_name] = df[sheet_name].assign(Customer=customer_name) #Add a new column with the customer_name value
-    return df
+    # Replace NaTType values with default value
+    clean_df['Approval_Date'].fillna('1970-01-01', inplace=True)
+    clean_df['Status_Change_Date'].fillna('1970-01-01', inplace=True)
+    clean_df['Patch_Products'].fillna('', inplace=True)
 
-# Function to pull each patch status table from the sheets
-def extract_table(df):
-    """
-    Extracts data from each sheet of the input dataframe.
+    # Remove any commas in the Patch_Category column
+    clean_df['Patch_Products'] = clean_df['Patch_Products'].replace(',', ' ', regex=True)
 
-    Args:
-    df (Pandas dataframe): The dataframe containing the data from the Excel file.
-
-    Returns:
-    sheet_dfs (dictionary): A dictionary containing the extracted data.
-    """
-    print("Extracting tables from each sheet")
-    statuses = ['Aborted', 'Failed', 'In Progress', 'Installed With Errors', 'Not Installed', 'Installed']
-    sheet_dfs = {}
-
-    for sheet_name in df:
-        for status in statuses:
-            startIndex = df[sheet_name][df[sheet_name]['Device Class'] == 'Patch Status: {}'.format(status)].index.tolist()
-            endIndex = df[sheet_name][df[sheet_name]['Device Class'] == '{} Patch Count: '.format(status)].index.tolist()
-
-            # Check that there is data for this status
-            if startIndex and endIndex:
-                sheet_dfs[sheet_name + '_' + status] = df[sheet_name].iloc[startIndex[0]+2:endIndex[0]].copy()
-                sheet_dfs[sheet_name + '_' + status]['Patch Status'] = status
-                sheet_dfs[sheet_name + '_' + status] = sheet_dfs[sheet_name + '_' + status].fillna(method='ffill')
-    return sheet_dfs
-
-# Function to merge dataframes into one dataframe
-def merge_df(df, source, type, date):
-    """
-    Merges data from each dataframe of the input dataframe.
-
-    Args:
-    df (Pandas dataframe): The dataframe containing the data from the Excel file.
-    source(string): The automatically pulled server source taken from the filename in import_excel()
-    type(string): The automatically pulled endpoint type taken from the filename in import_excel()
-    date(string):The automatically pulled date taken from the filename in import_excel()
-
-    Returns:
-    merged_df (dataframe): A dataframe containing the merged data.
-    """
-    print("Merging each dataframe from df into one merged_df")
-    merged_df = pd.concat(df) #Concat all data into one df
-    merged_df = merged_df.iloc[:, :7] #Drop all columns after 7
-    merged_df = merged_df.assign(Source=source) # Add a column with the server source
-    merged_df = merged_df.assign(Type=type) # Add a column with the endpoint type
-    merged_df = merged_df.assign(Date=date) # Add a column with the date
-    
-    return (merged_df)
-
-# Filter clients from the dataset
-def filter_df(master_df, customer_filter):
-    print("Filtering dataframe")
-    for customer in customer_filter:
-
-        print("Filtering out", customer)
-        # filter out rows where Customer is in customer filter file
-        master_df = master_df.loc[master_df['Customer'] != customer]
-    return(master_df)
-
-# Save file
-def output_file(master_df, date):
-    """
-    Saves data from dataframe into csv file.
-
-    Args:
-    df (Pandas dataframe): The dataframe containing the data from the Excel file.
-
-    Returns:
-    None
-    """
-    print("Please select a filepath to save the output file to.")
-    # Prompt user to select file path and name
-    default_directory = str(Path.home().joinpath('Desktop'))
-    output_file_path = filedialog.asksaveasfilename(initialdir=default_directory, initialfile=f"Patch Status - {date}.xlsx",defaultextension='.xlsx', title="Save The XLSX File", filetypes=(('XLSX Files', '*.xlsx'), ('All files', '*.*')))
-    print("Using", output_file_path, "to save the file to.")
-
-    # create an Excel writer object
-    writer = pd.ExcelWriter(output_file_path, engine='xlsxwriter')
-
-    # Save merged dataframe to XLSX file
-    try:
-        print("Attempting to save file:", output_file_path)
-        master_df.to_excel(writer, sheet_name='Data', index=False)
-        print("Output file saved successfully")
-    except FileNotFoundError:
-        print("The specified file path could not be found. Please try again.")
-    except PermissionError:
-        print("You do not have permission to save a file at the specified location. Please try again with a different file path.")
-    except Exception as e:
-        print("An unexpected error occurred while saving the output file: {}".format(e))
-
-    # create a pivot table
-    print("Creating pivot table")
-    pivot_table = pd.pivot_table(master_df, values='Count', index=['Source', 'Type'], columns='Patch Status', aggfunc='sum', fill_value=0, margins=True)
-
-    # Pulling installed and all patch counts for each row
-    all_values = pivot_table['All'].drop(index='All')
-    installed_values = pivot_table['Installed'].drop(index='All')
-
-    # Calculate the percentage of installed values
-    percent_installed = installed_values / all_values * 100
-    # Format as percentage with no decimal places
-    percent_installed_str = percent_installed.map('{:.1f}%'.format)
-    pivot_table = pivot_table.assign(Percent_Installed=percent_installed_str)
-
-    # write the pivot table to a second sheet
-    print("Writing pivot table to second sheet.")
-    pivot_table.to_excel(writer, sheet_name='Summary')
-
-    writer.close()
-    print("File save complete.")
-
-    return
+    return clean_df
 
 # Save data to MariaDB
-def output_db(master_df):
+def output_db(master_df, date):
     # Create a connection object
     print("Connecting to MariaDB")
     conn = mariadb.connect(**db_config)
@@ -222,77 +84,79 @@ def output_db(master_df):
 
     # Execute an SQL query to create a table
     print("Creating table if one doesn't exist")
-    cursor.execute("CREATE TABLE IF NOT EXISTS my_table (id INT AUTO_INCREMENT PRIMARY KEY, date DATE, source VARCHAR(255), type VARCHAR(255), customer VARCHAR(255), device_class VARCHAR(255), device_name VARCHAR(255), patch_category VARCHAR(255), patch_status VARCHAR(255), count INT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS nable_table (id INT AUTO_INCREMENT PRIMARY KEY, customer_name VARCHAR(255), installation_status VARCHAR(255), device_class VARCHAR(255), device_name VARCHAR(255), patch_name VARCHAR(255), patch_products VARCHAR(255), patch_category VARCHAR(1255), publish_date DATE, approval_status VARCHAR(255), approval_date DATE, status_change_date DATE, source VARCHAR(255), report_date DATE)")
 
-    # Loop through the data and insert each row into the table
-    print("Saving data to MariaDB")
-    for index, row in tqdm(master_df.iterrows()):
-        Date = row['Date']
-        Source = row['Source']
-        date = row['Date']
-        source = row['Source']
-        type = row['Type']
-        customer = row['Customer']
-        device_class = row['Device Class']
-        device_name = row['Device Name']
-        patch_category = row['Patch Category']
-        patch_status = row['Patch Status']
-        count = row['Count']
-        cursor.execute("INSERT INTO my_table (date, source, type, customer, device_class, device_name, patch_category, patch_status, count) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (date, source, type, customer, device_class, device_name, patch_category, patch_status, count))
+    # Prepare the insert query
+    insert_query = "INSERT INTO nable_table (customer_name, installation_status, device_class, device_name, patch_name, patch_products, patch_category, publish_date, approval_status, approval_date, status_change_date, source, report_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
-    #Date	Source	Type	Customer	Device Class	Device Name	Patch Category	Patch Status	Count
 
-    # Commit the changes to the database
-    print("Commiting data")
-    conn.commit()
+    # Execute the query to search for the date in the report_date column
+    print("Searching DB to see if data already exists based on report date")
+    cursor.execute("SELECT * FROM nable_table WHERE report_date = %s", (date,))
+
+    # Fetch the results of the query
+    result = cursor.fetchall()
+
+    # Check if the date was found in the report_date column
+    if result:
+        # Code to execute if the date was found
+        print("The date was found in the report_date column!")
+    else:
+        # Code to execute if the date was not found
+        print("The date was not found in the report_date column.")
+
+        # Loop through the data and insert each row into the table in batches
+        print("Saving data to MariaDB")
+        pd.options.mode.chained_assignment = None  # default='warn'
+        batch_size = 1000
+        for i in tqdm(range(0, len(master_df), batch_size)):
+            batch_df = master_df.iloc[i:i+batch_size]
+
+            # Convert the date columns to datetime objects
+            batch_df['Publish_Date'] = pd.to_datetime(batch_df['Publish_Date'], format='%b %d, %Y', errors='coerce').dt.date
+            batch_df['Approval_Date'] = pd.to_datetime(batch_df['Approval_Date'], format='%b %d, %Y', errors='coerce').dt.date
+            batch_df['Status_Change_Date'] = pd.to_datetime(batch_df['Status_Change_Date'], format='%b %d, %Y', errors='coerce').dt.date
+
+            # Convert the dataframe to a list of tuples for batch insert
+            batch_values = batch_df.to_records(index=False).tolist()
+
+            # Execute the batch insert
+            cursor.executemany(insert_query, batch_values)
+
+        # Commit the changes to the database
+        print("Committing data")
+        conn.commit()
 
     # Close the cursor and connection objects
     print("Exiting from MariaDB")
     cursor.close()
     conn.close()
 
-
     return
 
 # Main funtion of program
 if __name__ == '__main__':
 
-    # Prompt user for directory path of excel files to import
+    # Prompt user for directory path of csv files to import
     dir_path = get_dir()
 
-    # Look at each file in the folder, if they are an excel file, process the file.
+    # Look at each file in the folder, if they are a csv file, process the file.
     for file in os.listdir(dir_path):
-        print("Looking at file:" , file)
-        if file.endswith('.xlsx'):
+        print(f"Looking at file: {file}")
+        if file.endswith('.csv'):
             file_path = os.path.join(dir_path, file)
-            # Import excel file
-            df, source, type, date = import_excel(file)
+            # Import csv file
+            import_df, source, date = import_csv(file)
 
-            # Clean up each sheet
-            df = clean_data(df)
+            # Clean csv file
+            clean_df = cleanup_df(import_df, source, date)
 
-            # Extract tables from each sheet
-            df = extract_table(df)
+            # Append data to a single dataframe
+            master_df = pd.concat([master_df, clean_df])
 
-            # Merge all dataframes from df into a single dataframe called merged_df
-            merged_df = merge_df(df, source, type, date)
-
-            # Append each file's merged_df into a single master_df
-            print("Appending each file's merged_df into the master_df")
-            master_df = pd.concat([master_df, merged_df])
         else:
-            print("File does not end with .xlsx, skipping to next file")
+            print("File does not end with .csv, skipping to next file")
             continue
 
-    #Filter out clients
-    master_df = filter_df(master_df, customer_filter)
-
-    # Save data into csv file
-    output_file(master_df, date)
-
-    
-    # Save data to MariaDB
-    output_db(master_df)
-    
-
-    input("Script completed, press enter to close the window...")
+    # Export data to MariaDB
+    output_db(master_df, date)
